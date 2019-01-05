1a0acd5900a979daa7c5d9566257fcd814cf6375
Serdar Mutlu <serdar.mutlu@ozguryazilim.com.tr>
2019-01-06 02:42:24 +0300

    token'i body'de gönderme

--- src/main/java/com/nku/hospitalreporting/hospitalreportingservice/security/JWTAuthenticationFilter.java
+++ src/main/java/com/nku/hospitalreporting/hospitalreportingservice/security/JWTAuthenticationFilter.java
@@ -62,6 +62,9 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .withSubject(((User) auth.getPrincipal()).getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .sign(HMAC512(SECRET.getBytes()));
-        res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+        //res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+        res.getWriter().write(TOKEN_PREFIX + token);
+        res.getWriter().flush();
+        res.getWriter().close();
     }
 }
